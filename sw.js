b55a5ec5037554e8f606c4c1eec3295a0e1ae58d
vemod- <thomas.allin.vemod@gmail.com>
2025-05-24 15:54:39 +0200

    Web app

---
+++ sw.js
@@ -0,0 +1,13 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("inkops-cache").then(cache => {
+      return cache.addAll([".", "index.html", "manifest.json", "icon-192.png", "icon-512.png"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
